6b50be8499281e96d4a8618c6d008d7e2919aed1
liyao <liyao@corpvip.com>
2024-12-27 10:08:33 +0800

    添加统计

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -1,7 +1,12 @@
+import Analytics from '@/components/Analytics'
+
 export default async function Layout({ children, params }) {
   const { lang } = await params;
   return (
     <html lang={lang}>
+      <head>
+        <Analytics />
+      </head>
       <body>{children}</body>
     </html>
   );
